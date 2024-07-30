f941e7a76fc1fcb5ebd8e280ad746337a270bade
rambe98 <81672761+rambe98@users.noreply.github.com>
2024-07-30 17:44:47 +0900

    Day1

---
+++ EX_0730_first/src/Ex_print/pt01.java
@@ -0,0 +1,43 @@
+package Ex_print;
+
+public class pt01 {
+
+	public static void main(String[] args) {
+/*		System.out.print("집갈까");
+		System.out.println();
+		System.out.print("배부르다");
+		System.out.println();
+		System.out.printf("이렇다할");
+------------------------------------------------------
+		System.out.println("집갈까");
+		System.out.println("배부르다");
+		System.out.println("이렇다할");
+------------------------------------------------------
+		System.out.printf("저는 대학교 %d학년에 재학중입니다.",3);
+		System.out.println();
+		System.out.printf("%d은 첫 번째, %f은 두 번째, %s은 세 번째.",1,2.0,"셋");
+		// \n: 개행문자(줄바꿈)
+		System.out.println();
+		System.out.printf("%d은 첫 번째, \n%f은 두 번째, \n%s은 세 번째.",1,2.0,"셋");
+------------------------------------------------------
+		// 출력값의 정렬
+		// %3d -> 주어진 숫자 만큼 바이트 확보, 오른쪽 정렬로 출력
+		System.out.printf("%15d\n", 1);
+		System.out.printf("%5d\n",1);
+		System.out.printf("%5d\n",12);
+		System.out.printf("%5d\n",123);
+		System.out.printf("%5d\n",1234);
+		System.out.printf("%5d\n",12345);
+------------------------------------------------------
+	
+		//실수의 정렬
+		//%.2f >> 몇번째 자리의 소수점까지 나타낼건지 숫자로 결정(이후 자리수는 반올림)
+		System.out.printf("%.1f\n",1.1234567);
+		System.out.printf("%.2f\n",1.1234567);
+		System.out.printf("%.3f\n",1.1234567);
+		System.out.printf("%.4f\n",1.1234567);
+		System.out.printf("%.5f\n",1.1234567);
+------------------------------------------------------
+*/					
+	}
+}
