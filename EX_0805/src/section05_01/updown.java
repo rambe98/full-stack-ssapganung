503a9ceb7f3d428d6d7e8771fc625d0d3315a91c
rambe98 <81672761+rambe98@users.noreply.github.com>
2024-08-06 09:50:24 +0900

    Update updown.java
    주석 추가

--- EX_0805/src/section05_01/updown.java
+++ EX_0805/src/section05_01/updown.java
@@ -2,9 +2,8 @@ package section05_01;
 import java.util.Random;
 
 public class updown {
-	Random rnd = new Random();
-	
-	int rnum = rnd.nextInt(50)+1;
+	Random rnd = new Random();// 난수를 생성하기 위한 Random클래스의 객체
+	int rnum = rnd.nextInt(50)+1;// 1~ 50 사이의 난수 생성
 	int count = 1;
 	
 	public String check(int number) {
