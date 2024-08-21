90077b7558a8e6f498bb8cab9bb55c6c750df5a9
rambe98 <81672761+rambe98@users.noreply.github.com>
2024-08-21 11:10:54 +0900

    Update Unit.java

--- Ex_0821_OhGyuHan/src/ex03/Unit.java
+++ Ex_0821_OhGyuHan/src/ex03/Unit.java
@@ -8,10 +8,10 @@ public class Unit {
 class Marine extends Unit{// 보병
     void stimPack() { /* 스팀팩을 사용한다.*/}
 }
-class Tank { // 탱크
+class Tank extends Unit{ // 탱크
     void changeMode() { /* 공격모드를 변환한다. */}
 }
-class Dropship { // 수송선
+class Dropship extends Unit{ // 수송선
     void load() { /* 선택된 대상을 태운다.*/ }
     void unload() { /* 선택된 대상을 내린다..*/ }
 }
