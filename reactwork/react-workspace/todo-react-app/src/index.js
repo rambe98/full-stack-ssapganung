e2f95b46ec76edc8c85c2b9ded0606b2e5da45d5
rambe98 <rbgks33@gmail.com>
2024-09-11 17:30:36 +0900

    react work
    day1 update

---
+++ reactwork/react-workspace/todo-react-app/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';//app 컴포넌트를 import
+import reportWebVitals from './reportWebVitals';
+
+/*
+ReactDOM.render{
+<React.StrictMode>
+  <App />
+</React.StrictMode>
+document.getElementById('root')
+}
+
+React18부터 React.render()가 아닌 creatRoot() 를 먼저 사용한다.
+<컴포넌트 명/>을 이용해 컴포넌트를 사용한다.
+*/
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(//reactDom이 내부 컴포넌트 를 root 엘리먼트에 render함
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
