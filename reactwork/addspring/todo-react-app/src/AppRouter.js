3bc561a63927619f065d7ba9b7384369c702b496
rambe98 <rbgks33@gmail.com>
2024-10-10 17:59:30 +0900

    todo리엑트 및 api구현

--- reactwork/addspring/todo-react-app/src/AppRouter.js
+++ reactwork/addspring/todo-react-app/src/AppRouter.js
@@ -16,7 +16,9 @@ function Copyright(){
     );
 }
 
-function AppRouter(){
+export default function AppRouter(){
+    
+    
     return(
         <div>
             <BrowserRouter>
@@ -32,5 +34,5 @@ function AppRouter(){
         </div>
     )
 }
-export default AppRouter;
+
 
