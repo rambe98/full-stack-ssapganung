82bfb424c3bd1a52478e0ac2fc90bd542fef21ac
rambe98 <rbgks33@gmail.com>
2024-11-11 17:03:34 +0900

    simple chat

--- reactnative/native-navigation/src/App.js
+++ reactnative/native-navigation/src/App.js
@@ -5,14 +5,16 @@ import StackNavigation from '../navigations/Stack';
 import TabNavigation from '../navigations/Tab';
 import BookStack from '../navigations/BookStack';
 import UserStack from '../navigations/UserStack';
+import DrawerNavigation from '../navigations/Drawer';
 
 const App = () => {
   return (
     <NavigationContainer>
         {/* <StackNavigation/> 
         <BookStack/>
-        <UserStack/>*/}
-        <TabNavigation/>
+        <UserStack/>
+        <TabNavigation/>*/}
+        <DrawerNavigation />
     </NavigationContainer>
   );
 };
